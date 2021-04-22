a89d05f52c8ae33b2ea03a2fd6c4336af4014c47
DK <comododk@gmail.com>
2021-04-22 14:03:21 -0300

    SSG

--- podcastrnext/src/pages/index.tsx
+++ podcastrnext/src/pages/index.tsx
@@ -1,5 +1,26 @@
-export default function Home() {
+export default function Home(props) {
+/* //Usando SPA
+    useEffect(() => {
+    fetch('http://localhost:3333/episodes')
+      .then(response => response.json())
+      .then(data => console.log(data))
+  }, []); 
+*/
+
   return (
     <h1>Index</h1>
   )
+}
+
+//SSR = getServerSideProps; SSG = getStaticProps
+export async function getStaticProps(){
+  const response = await fetch('http://localhost:3333/episodes')
+  const dada = await response.json()
+
+  return {
+    props: {
+      episodes: dada, 
+    }, 
+    revalidate: 60 * 60 * 8;
+  }
 }
